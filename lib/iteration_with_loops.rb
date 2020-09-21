85bdf4fa7939899b138b35a12be29e40eef35211
Meredith McIntosh <meredith@mcintosh.io>
2020-09-21 18:45:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,12 +7,25 @@ mixed_data = [
 ]
 
 def join_nested_strings(mixed_data)
-  index = 0
-  new_sentence = []
-  while index < mixed_data.count do
-    new_sentence << mixed_data[index].is_a?(String)
+  current_level = 0
+  new_sentence = [] # new empty array
+  while current_level < mixed_data.count do # will run 3 times because there are 3 top level elements
+    # make a new counter for this level
+    p "first level works"
+    element_counter = 0
+    # loop through all the elements in the array at the current level
+    while element_counter < mixed_data[current_level].count do
+      p "second level works"
+      # want check if the current element is a string
+      if mixed_data[current_level][element_counter].is_a?(String)
+        # the current element is a string add it to the new sentence
+        new_sentence << mixed_data[current_level][element_counter]
+      end
+      element_counter += 1
+    end
+    current_level += 1 # increment our counter by one so we know which row we are looking at
   end
-  new_sentence
+  p new_sentence
 end
 join_nested_strings(mixed_data)
 
