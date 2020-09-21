8b8605e91d6d40802745c9be9fb9fe92dcb34e9f
Meredith McIntosh <meredith@mcintosh.io>
2020-09-21 16:41:07 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,24 @@
-def join_nested_strings(src)
+require "pry"
+
+mixed_data = [
+  ["The", 4, "quick"],
+  [-1, "brown", "fox", 30],
+  ["studied", 101, 233, "Ruby"]
+]
+
+def join_nested_strings(mixed_data)
+  index = 0
+  new_sentence = []
+  while index < mixed_data.count do
+    new_sentence << mixed_data[index].is_a?(String)
+  end
+  new_sentence
+end
+join_nested_strings(mixed_data)
+
+
+
+
+
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-end
